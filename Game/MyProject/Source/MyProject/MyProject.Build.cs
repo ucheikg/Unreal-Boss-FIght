4c59fbe451f3e704014e404d39a30c338cf2c42b
UCHEOMA EMELONYE <UE315957@falmouth.ac.uk>
2025-11-10 12:56:44 +0000

    working on behaviour trees

--- Game/MyProject/Source/MyProject/MyProject.Build.cs
+++ Game/MyProject/Source/MyProject/MyProject.Build.cs
@@ -1,6 +1,7 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 using UnrealBuildTool;
+using UnrealBuildTool.Rules;
 
 public class MyProject : ModuleRules
 {
@@ -8,7 +9,7 @@ public class MyProject : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "AIModule", "StateTreeModule", "GameplayStateTreeModule", "UMG", "Slate", "NavigationSystem"});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
