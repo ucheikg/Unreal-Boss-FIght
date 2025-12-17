3304a9644b248342eabc599316c9a5aefc9d4a13
UCHEOMA EMELONYE <ue315957@falmouth.ac.uk>
2025-12-17 21:31:24 +0000

    Update Tyson_BTTask_BlackboardBase.cpp

--- Game/MyProject/Source/MyProject/Tyson_BTTask_BlackboardBase.cpp
+++ Game/MyProject/Source/MyProject/Tyson_BTTask_BlackboardBase.cpp
@@ -4,7 +4,7 @@
 #include "Tyson_BTTask_BlackboardBase.h"
 #include "AiController.h"
 #include "Tyson_Character.h"
-
+// gives node a name
 UTyson_BTTask_BlackboardBase::UTyson_BTTask_BlackboardBase()
 {
     NodeName = TEXT("Attack Player"); // This name shows up in the Behavior Tree editor
@@ -14,13 +14,13 @@ UTyson_BTTask_BlackboardBase::UTyson_BTTask_BlackboardBase()
 
 EBTNodeResult::Type UTyson_BTTask_BlackboardBase::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
-
+// checks if Pawn has AI controlled
     AAIController* AIController = OwnerComp.GetAIOwner();
     if (!AIController) return EBTNodeResult::Failed;
-
+// cast boss
     ATyson_Character* Boss = Cast<ATyson_Character>(AIController->GetPawn());
     if (!Boss) return EBTNodeResult::Failed;
-    
+    // calls right hook function in boss
     Boss->rightHook();
 
     return EBTNodeResult::Succeeded;
