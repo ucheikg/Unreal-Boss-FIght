a3afd9b577131f742e1b5b623156f45e51f8a73e
UCHEOMA EMELONYE <ue315957@falmouth.ac.uk>
2025-12-17 20:51:45 +0000

    added comments

--- Game/MyProject/Source/MyProject/LongRangeBTTask_BlackboardBase.cpp
+++ Game/MyProject/Source/MyProject/LongRangeBTTask_BlackboardBase.cpp
@@ -5,21 +5,23 @@
 #include "Tyson_Character.h"
 #include "AiController.h"
 
+// //give node a name
 ULongRangeBTTask_BlackboardBase::ULongRangeBTTask_BlackboardBase()
 {
-
 	NodeName = TEXT("Long Range Attack");
 
 }
 
+
 EBTNodeResult::Type ULongRangeBTTask_BlackboardBase::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
+//checks if the boss is controlled by AI if not return failed if it is cast the boss
     AAIController* AIController = OwnerComp.GetAIOwner();
     if (!AIController) return EBTNodeResult::Failed;
 
     ATyson_Character* Boss = Cast<ATyson_Character>(AIController->GetPawn());
     if (!Boss) return EBTNodeResult::Failed;
-
+// call the bosses left hook function and returns succeded
     Boss->leftHook();
 
     return EBTNodeResult::Succeeded;
